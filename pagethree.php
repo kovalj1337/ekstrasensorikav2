a5a6d6298d3086aab19864823173f2ffb37f72f7
kovalj1337 <vanaplayers@gmail.com>
2024-08-09 17:58:18 +0300

    ddd

--- pagethree.php
+++ pagethree.php
@@ -6,26 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    <?php
     session_start();
     $rand = $_SESSION["random"];
     $choice = $_POST["choice"];
+    var_dump($rand);
     if(isset($_SESSION["checknumb"])){
         $checkNumb = $_SESSION["checknumb"];
     }else{
         $_SESSION["checknumb"] = $checkNumb = [];
     }
+    $checkNumb = [];
     if(isset($_POST["choice"])){
         $numbSelect = $_POST["choice"];
     }else{
         $numbSelect = 0;
+    }
+    array_push($_SESSION["checknumb"], $numbSelect);
+    if(!in_array($numbSelect, $checkNumb)){
+        $checkNumb[] = $numbSelect;
     }
     if($rand == $choice){
         echo("Ви вибрали првильне число!");
     }else{
         if ($rand >= $choice && $rand <= $choice + 5) {
             $numb = ceil($rand / 10) * 10 - 4;
-            echo("$numb dd d ");
+            echo("$choice dd d ");
         } else {
             $numb = floor($rand / 10) * 10 + 1;
             echo($numb);
@@ -36,7 +42,11 @@
             <select name='choice'>
         ");
         for ($i = $numb; $i < $numb + 5; $i++) {
-            echo ("<option>$i</option>");
+            if(!in_array($i, $_SESSION["checknumb"])){
+                echo("<option value='$i'>$i</option>");
+            }else{
+                echo("<option value='$i' disabled>$i неактивно</option>");
+            }
         }
         echo ("
         </select>
