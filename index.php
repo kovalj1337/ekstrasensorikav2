2b210299bfc2375b417c5ad3df9ae2bda7fa3011
kovalj1337 <vanaplayers@gmail.com>
2024-08-09 21:30:10 +0400

    lol

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <?php 
-    session_start();
-    $_SESSION["randomaizer"] = $rand = rand(1,100);
-    echo("Число загадано");
-    ?>
-    <form action="pagetwo.php" method="post">
-        <select name="diapazon" id="">
-        <?php 
-            for($i = 1; $i <= 100; $i += 10){
-                $j = $i + 9;
-                echo"<option value='$i'>$i - $j</option>";
-            }
+    <main>
+        <?php
+        session_start();
+        $_SESSION["randomaizer"] = $rand = rand(1, 100);
+        echo ("Число загадано");
         ?>
-        </select>
-        <button type="submit">Сабміт</button>
-    </form>
+        <form action="pagetwo.php" method="post">
+            <select name="diapazon" id="">
+                <?php
+                for ($i = 1; $i <= 100; $i += 10) {
+                    $j = $i + 9;
+                    echo "<option value='$i'>$i - $j</option>";
+                }
+                ?>
+            </select>
+            <button type="submit">Сабміт</button>
+        </form>
+    </main>
 </body>
+
 </html>
--- pagethree.php
+++ pagethree.php
@@ -1,58 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <?php
     session_start();
-    $rand = $_SESSION["random"];
-    $choice = $_POST["choice"];
-    var_dump($rand);
-    if(isset($_SESSION["checknumb"])){
-        $checkNumb = $_SESSION["checknumb"];
-    }else{
-        $_SESSION["checknumb"] = $checkNumb = [];
-    }
-    $checkNumb = [];
-    if(isset($_POST["choice"])){
-        $numbSelect = $_POST["choice"];
-    }else{
-        $numbSelect = 0;
-    }
-    array_push($_SESSION["checknumb"], $numbSelect);
-    if(!in_array($numbSelect, $checkNumb)){
-        $checkNumb[] = $numbSelect;
-    }
-    if($rand == $choice){
-        echo("Ви вибрали првильне число!");
-    }else{
-        if ($rand >= $choice && $rand <= $choice + 5) {
-            $numb = ceil($rand / 10) * 10 - 4;
-            echo("$choice dd d ");
-        } else {
-            $numb = floor($rand / 10) * 10 + 1;
-            echo($numb);
-        }
-        // $numb = floor($rand / 10) * 10 + 1;
-        var_dump($numb);
-        echo ("<form action='pagethree.php' method='post'>
-            <select name='choice'>
-        ");
-        for ($i = $numb; $i < $numb + 5; $i++) {
-            if(!in_array($i, $_SESSION["checknumb"])){
-                echo("<option value='$i'>$i</option>");
-            }else{
-                echo("<option value='$i' disabled>$i неактивно</option>");
+    function cikl($numb)
+    {
+        echo ("
+        <form action='pagethree.php' method='post'>
+            <select name='choice'>"
+        );
+        $countI = $numb + 4;
+        for ($numb; $numb <= $countI; $numb++) {
+            if (!in_array($numb, $_SESSION["checknumb"])) {
+                echo ("<option value='$numb'>$numb</option>");
+            } else {
+                echo ("<option value='$numb' disabled>$numb неактивно</option>");
             }
         }
+        // $numb = 0;
         echo ("
         </select>
         <button type='submit'>Sabmit</button>
-        </form>");
+        </form>"
+        );
     }
+    ;
+    // session_destroy();
+    $rand = $_SESSION["random"];
+    $choice = $_POST["choice"];
+    var_dump($rand);
+    if (isset($_SESSION["checknumb"])) {
+        $checkNumb = $_SESSION["checknumb"];
+    } else {
+        $_SESSION["checknumb"] = $checkNumb = [];
+    }
+    $checkNumb = [];
+    if (isset($_POST["choice"])) {
+        $numbSelect = $_POST["choice"];
+    } else {
+        $numbSelect = 0;
+    }
+    array_push($_SESSION["checknumb"], $numbSelect);
+    if (!in_array($numbSelect, $checkNumb)) {
+        $checkNumb[] = $numbSelect;
+    }
+    if ($rand == $choice) {
+        echo ("Ви вибрали првильне число!");
+    } else {
+        $start = floor($rand / 10) * 10 + 5;
+        echo ($start);
+        if ($rand <= $start) {
+            $numb = floor($rand / 10) * 10 + 1;
+            if(isset($numbSelect)){
+                if($numbSelect < $rand){
+                    echo("Загадане число більше твого");
+                }else{
+                    echo("Загадане число меньше твого");
+                }
+            }
+            cikl($numb);
+        } else if ($rand > $start) {
+            $numb = ceil($rand / 10) * 10 - 4;
+            if(isset($numbSelect)){
+                if($numbSelect < $rand){
+                    echo("Загадане число більше твого");
+                }else{
+                    echo("Загадане число меньше твого");
+                }
+            cikl($numb);
+        }
+        // var_dump($numb);
+        // if ($rand >= $choice && $rand <= $choice) {
+        // } else {
+        // }
+        // $numb = floor($rand / 10) * 10 + 1;
+        // echo("$numb");
+    
+    }
+}
     ?>
 </body>
+
 </html>
